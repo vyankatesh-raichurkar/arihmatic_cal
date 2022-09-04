7f179d3e3c1608b821ba38818b685e80cc581b96
dev_love_per <76610148+vyankatesh-raichurkar@users.noreply.github.com>
2022-09-04 16:35:59 +0530

    Add files via upload

---
+++ arithmatic_cal.java
@@ -0,0 +1,16 @@
+class arithmatic_cal
+{  
+   public static void main(String args[])
+   {
+    int a=25,b=30,c;
+    System.out.println(a+" and "+b);
+    c=a+b;
+    System.out.println("addition is ="+c);
+    c=a-b;
+    System.out.println("substraction is ="+c);
+    c=a*b;
+    System.out.println("multiplication is ="+c);
+    c=a/b;
+    System.out.println("division is ="+c);
+   }
+}
